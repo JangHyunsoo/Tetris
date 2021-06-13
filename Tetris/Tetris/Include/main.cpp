812205b47c18e9ba9e9efe58ac76b7f9e0a213a4
JangHyunsoo <hunsoo911@gmail.com>
2021-06-13 18:03:43 +0900

    tag : change user config

--- Tetris/Tetris/Include/main.cpp
+++ Tetris/Tetris/Include/main.cpp
@@ -1,6 +1,11 @@
 #include "common.h"
 
-int main() {
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR IpCmdLine,
+	_In_ int nCmdShow)
+{
+
 
 	return 0;
 }
